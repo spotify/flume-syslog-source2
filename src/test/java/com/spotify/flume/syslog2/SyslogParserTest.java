9f733b96a02f00a2e11afd34fb930d7672461a79
Tommie Gannert <tommie@gannert.se>
2012-05-03 17:48:52 +0200

    Fix unit tests that depends on current year. #1
    Since BSD syslog has a timestamp format without year we have to use the
    current year for some of the unit tests.

--- src/test/java/com/spotify/flume/syslog2/SyslogParserTest.java
+++ src/test/java/com/spotify/flume/syslog2/SyslogParserTest.java
@@ -58,10 +58,10 @@ public class SyslogParserTest {
 		InputStream in = toInputStream(msg);
 		SyslogParser p = new SyslogParser(in, true, ENCODING);
 		Event e = p.readEvent();
-		
+
 		assertEventEquals(target, e);
 	}
-	
+
 	@DataProvider(name = "messages")
 	public Object[][] createValidMessageData() throws Exception {
 		// XXX: The BSD timestamp tests will fail if we run them while
@@ -69,78 +69,78 @@ public class SyslogParserTest {
 		return new Object[][] {
 			{ "", null },
 			// BSD date formats
-			{ "<11>Oct  5 12:23:34 hostname tag: hello world", createEventImpl("hello world", false, Event.Priority.ERROR, new Object[][] {
+			{ "<11>Oct  5 12:23:34 hostname tag: hello world", createEventImpl("hello world", false, true, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// Not really valid, but want to check double-digit day.
-			{ "<11>Oct 05 12:23:34 hostname tag: hello world", createEventImpl("hello world", false, Event.Priority.ERROR, new Object[][] {
+			{ "<11>Oct 05 12:23:34 hostname tag: hello world", createEventImpl("hello world", false, true, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// Priority
-			{ "<1>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.FATAL, new Object[][] {
+			{ "<1>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.FATAL, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 0 } },
 				{ "syslogseverity", new byte[] { 1 } } }) },
-			{ "<111>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.DEBUG, new Object[][] {
+			{ "<111>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.DEBUG, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 13 } },
 				{ "syslogseverity", new byte[] { 7 } } }) },
 			// ISO date formats
-			{ "<11>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05 12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>2011-10-05T12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34Z hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>2011-10-05T12:23:34.567Z hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34.567Z hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// Time zones
-			{ "<11>2011-10-05T12:23:34+00 hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34+00 hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>2011-10-05T12:23:34+00:00 hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34+00:00 hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>2011-10-05T12:23:34-00 hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34-00 hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>2011-10-05T12:23:34-00:00 hostname tag: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34-00:00 hostname tag: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// PID
-			{ "<11>2011-10-05T12:23:34Z hostname tag[pid]: hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>2011-10-05T12:23:34Z hostname tag[pid]: hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "tag" },
 				{ "syslog.procId", "pid" },
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// RFC 5424
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - - hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - - hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>1 2011-10-05T12:23:34Z hostname app proc msg - hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname app proc msg - hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.appname", "app" },
 				{ "syslog.procId", "proc" },
 				{ "syslog.msgId", "msg" },
@@ -148,56 +148,56 @@ public class SyslogParserTest {
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 			// Structured data
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"\"] hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"\"] hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.sd", "[a b=\"\"]" },
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\"] hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\"] hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.sd", "[a b=\"c\"]" },
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a@123 b=\"c\"] hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a@123 b=\"c\"] hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.sd", "[a@123 b=\"c\"]" },
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\" d=\"e\"] hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\" d=\"e\"] hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.sd", "[a b=\"c\" d=\"e\"]" },
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
-			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\"][aa bb=\"cc\"] hello world", createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+			{ "<11>1 2011-10-05T12:23:34Z hostname - - - [a b=\"c\"][aa bb=\"cc\"] hello world", createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.sd", "[a b=\"c\"][aa bb=\"cc\"]" },
 				{ "syslog.version", new byte[] { 1 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }) },
 		};
 	}
-	
+
 	@Test()
 	public void testReadEventNoParseTag() throws Exception {
 		InputStream in = toInputStream("<11>2011-10-05T12:23:34Z hostname tag[pid]: hello world");
 		SyslogParser p = new SyslogParser(in, false, ENCODING);
-		
-		assertEventEquals(createEventImpl("tag[pid]: hello world", true, Event.Priority.ERROR, new Object[][] {
+
+		assertEventEquals(createEventImpl("tag[pid]: hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }), p.readEvent());
 	}
-	
+
 	@Test()
 	public void testReadEventNoTag() throws Exception {
 		InputStream in = toInputStream("<11>2011-10-05T12:23:34Z hostname hello world");
 		SyslogParser p = new SyslogParser(in, false, ENCODING);
-		
-		assertEventEquals(createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+
+		assertEventEquals(createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }), p.readEvent());
 	}
-	
+
 	@Test(dataProvider = "invalid-messages", expectedExceptions={ IOException.class })
 	public void testReadInvalidEvent(String msg) throws Exception {
 		InputStream in = toInputStream(msg);
@@ -205,7 +205,7 @@ public class SyslogParserTest {
 
 		p.readEvent();
 	}
-	
+
 	@DataProvider(name = "invalid-messages")
 	public Object[][] createInvalidMessageData() {
 		return new Object[][] {
@@ -217,38 +217,48 @@ public class SyslogParserTest {
 			{ "<11>2 2011-10-05T12:23:34-00:00 hostname tag: hello world" },
 		};
 	}
-	
+
 	@Test()
 	public void testLineBreaks() throws Exception {
 		InputStream in = toInputStream("<11>2011-10-05T12:23:34Z hostname hello world\n" +
 			"<11>2011-10-05T12:23:34Z hostname hello world 2\r\n" +
 			"<11>2011-10-05T12:23:34Z hostname hello world 3");
 		SyslogParser p = new SyslogParser(in, false, ENCODING);
-		
-		assertEventEquals(createEventImpl("hello world", true, Event.Priority.ERROR, new Object[][] {
+
+		assertEventEquals(createEventImpl("hello world", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }), p.readEvent());
 
-		assertEventEquals(createEventImpl("hello world 2", true, Event.Priority.ERROR, new Object[][] {
+		assertEventEquals(createEventImpl("hello world 2", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }), p.readEvent());
 
-		assertEventEquals(createEventImpl("hello world 3", true, Event.Priority.ERROR, new Object[][] {
+		assertEventEquals(createEventImpl("hello world 3", true, false, Event.Priority.ERROR, new Object[][] {
 				{ "syslog.version", new byte[] { 0 } },
 				{ "syslogfacility", new byte[] { 1 } },
 				{ "syslogseverity", new byte[] { 3 } } }), p.readEvent());
 
 		assertEventEquals(null, p.readEvent());
 	}
-	
-	EventImpl createEventImpl(String msg, boolean isUtc, Event.Priority pri, Object[][] fields) throws Exception {
-		Calendar cal = new GregorianCalendar(2011, 9, 5, 12, 23, 34);
+
+	/**
+	 * Create a mock EventImpl object.
+	 *
+	 * This is a convenience function hard-coding dates and other fields.
+	 *
+	 * @param msg the actual message body.
+	 * @param implicitYear true to use the current year, false to use the hard-coded value.
+	 * @param fields an array of key-value pairs.
+	**/
+	EventImpl createEventImpl(String msg, boolean isUtc, boolean implicitYear, Event.Priority pri, Object[][] fields) throws Exception {
+		int year = (implicitYear ? new GregorianCalendar().get(Calendar.YEAR) : 2011);
+		Calendar cal = new GregorianCalendar(year, 9, 5, 12, 23, 34);
 		Map<String, byte[]> f = new HashMap<String, byte[]>();
-		
+
 		if (isUtc) cal.setTimeZone(TimeZone.getTimeZone("UTC"));
-		
+
 		for (Object[] entry : fields) {
 			if (entry[1] instanceof byte[])
 				f.put((String) entry[0], (byte[]) entry[1]);
@@ -266,7 +276,7 @@ public class SyslogParserTest {
 		else
 			assertEquals(target.toString(), real.toString());
 	}
-	
+
 	protected InputStream toInputStream(String data) throws Exception {
 		return new ByteArrayInputStream(data.getBytes(ENCODING));
 	}
